8becbaedac33ef78339a4c349e5da7fd001bcdf9
LALKoO <mminem12@gmail.com>
2018-09-24 17:01:48 +0300

    reciver

--- app/reciver.js
+++ app/reciver.js
@@ -1,7 +1,9 @@
 var request = require('request');
 request.get('http://rabotyag.ga/games/RimWorld/RimWorld.txt', function (error, response, body) {
     if (!error && response.statusCode == 200) {
-        var csv = body;
+        var txt = body;
+        var str = txt.split("/n");
+        console.log(str[1], str[2]);
         // Continue with your processing here.
     }
 });
