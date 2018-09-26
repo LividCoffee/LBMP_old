25f2583908b1daab4adcd54bca5b921f22af5957
LALKoO <mminem12@gmail.com>
2018-09-26 10:02:51 +0300

    1

--- app/checkerload.js
+++ app/checkerload.js
@@ -3,11 +3,11 @@ var fs = require('fs');
 
 for (n = 1; n <= count; n++)
 {
-console.log(n);
-console.log(l_check[n]);
+//console.log(n);
+//console.log(l_check[n]);
 var file = fs.createWriteStream("./downloads/" + filename + "/" + filename + "_" + n + ".MD5");
 var request = http.get(l_check[n] , function(response)
 {
-  response.pipe(file);
+response.pipe(file, { end: false });
 });
 };
