d6fdc4721ebfc0d28408e0be82485c711aaa1960
LALKoO <mminem12@gmail.com>
2018-09-26 11:30:55 +0300

    1

--- app/checkerload.js
+++ app/checkerload.js
@@ -2,16 +2,17 @@ var http = require('http');
 var fs = require('fs');
 var async = require('async');
 
-for (n = 1; n <= count; n++)
-{
 //console.log(n);
 //console.log(l_check[n]);
-async.eachLimit(l_check[n], n, function(file, callback) {
-  var file = fs.createWriteStream("./downloads/" + filename + "/" + filename + "_" + n + ".MD5");
-  var request = http.get(l_check[n] , function(response)
-  {
-    response.pipe(file);
-  });
-}, function(err){
-});
+
+for (n = 1; n <= count; n++)
+{
+  async.eachLimit(l_check[n], n, function(file, callback) {
+    var file = fs.createWriteStream("./downloads/" + filename + "/" + filename + "_" + n + ".MD5");
+    var request = http.get(l_check[n] , function(response)
+    {
+      response.pipe(file);
+    });
+  },
+  function(err){});
 };
