94fa86e1deaa6f18ac4efc5c1f81115e92a8fbdf
LALKoO <mminem12@gmail.com>
2018-09-26 11:35:20 +0300

    download

--- app/download.js
+++ app/download.js
@@ -1,14 +1,15 @@
 var http = require('http');
 var fs = require('fs');
+var async = require('async');
 
-
-
-
-var file = fs.createWriteStream("./downloads/" + filename + "/RimWorld.txt");
-var request = http.get(Host + "/"  + filename + "/" + filename + ".txt", function(response)
-{
-  response.pipe(file);
-});
+async.eachLimit(l_url[n], n, function(file, callback) {
+  var file = fs.createWriteStream("./downloads/" + filename + "/" + filename + ".zip.00"+ n);
+  var request = http.get(l_url[n] , function(response)
+  {
+    response.pipe(file);
+  });
+},
+function(err){});
 
 //console.log(file);
 
