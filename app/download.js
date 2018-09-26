c80178252077fe418a1768a7e05b1646d62b0007
LALKoO <mminem12@gmail.com>
2018-09-26 11:37:58 +0300

    download

--- app/download.js
+++ app/download.js
@@ -3,7 +3,16 @@ var fs = require('fs');
 var async = require('async');
 
 async.eachLimit(l_url[n], n, function(file, callback) {
+
+  if (n<10)
   var file = fs.createWriteStream("./downloads/" + filename + "/" + filename + ".zip.00"+ n);
+  else if (n<100)
+  var file = fs.createWriteStream("./downloads/" + filename + "/" + filename + ".zip.0"+ n);
+  else if (n<1000)
+  var file = fs.createWriteStream("./downloads/" + filename + "/" + filename + ".zip."+ n);
+  else
+  console.log("error - n = " + n)
+
   var request = http.get(l_url[n] , function(response)
   {
     response.pipe(file);
